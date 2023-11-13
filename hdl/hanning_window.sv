c6dfeca750a93443680b4a883615c4757b44d198
Cynthia Zhang <zchcynthia@gmail.com>
2023-11-13 01:20:11 -0500

    hann window with testbench

---
+++ hdl/hanning_window.sv
@@ -0,0 +1,37 @@
+module hanning_window #(
+    parameter DATA_WIDTH = 8,
+    parameter SAMPLE_COUNT = 4096
+)(
+    input logic clk_in,
+    input logic rst_in,
+    input logic [DATA_WIDTH-1:0] in_sample,
+    output logic [DATA_WIDTH-1:0] out_sample,
+    output real cosout
+);
+    // array where each element is data type logic and width 8, and there are 4096 elements
+    //logic [DATA_WIDTH-1:0] window[SAMPLE_COUNT-1:0];
+    real PI;
+    assign PI = 3.141592;
+    logic [11:0] i;
+
+    //real cosout;
+    //assign cosout = $cos((2*PI*in_sample/256))*100;
+    assign cosout = 0.5 * (1 - $cos(2 * PI * i / (SAMPLE_COUNT - 1)));
+    
+    always_ff @(posedge clk_in) begin
+        if (rst_in) begin
+            out_sample <= 0;
+            i <= 0;
+        end else begin
+            i <= i+1;
+            // Hann window function
+            // for (int i = 0; i < SAMPLE_COUNT; i++) begin
+            //     //out_sample <= cosout;
+            //     out_sample <= cosout;
+            // end
+            out_sample <= cosout * in_sample;
+        end
+        //out_sample <= $cos(2*PI*in_sample);
+    end
+
+endmodule
---
+++ sim/hann_tb.sv
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module hann_tb();
+
+  logic clk_in;
+  logic rst_in;
+  logic [7:0] in_sample;
+  logic [7:0] out_sample;
+
+  hanning_window #(8,4096) uut (
+    .clk_in(clk_in),
+    .rst_in(rst_in),
+    .in_sample(in_sample),
+    .out_sample(out_sample)
+);
+
+  always begin
+      #5;  //every 5 ns switch...so period of clock is 10 ns...100 MHz clock
+      clk_in = !clk_in;
+  end
+  //initial block...this is our test simulation
+  initial begin
+    $dumpfile("hann_tb.vcd"); //file to store value change dump (vcd)
+    $dumpvars(0,hann_tb);
+    $display("Starting Sim"); //print nice message at start
+    clk_in = 0;
+    rst_in = 0;
+    in_sample = 0;
+    //out_sample = 0;
+    #10;
+    rst_in = 1;
+    #10;
+    rst_in = 0;
+    for (int i = 0; i<4096; i=i+1)begin
+      in_sample = i<<4;
+      #10;
+      end
+    $display("Simulation finished");
+    $finish;
+  end
+endmodule
+`default_nettype wire
