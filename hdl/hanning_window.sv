e2cbff6ea3d376c18feaa0ded6e34ea0d9529c14
zchalun <zcynthia@mit.edu>
2023-11-21 22:18:12 -0500

    mem file name, always needs changing

--- hdl/hanning_window.sv
+++ hdl/hanning_window.sv
@@ -50,7 +50,7 @@ module hanning_window #(
         .RAM_WIDTH(28),                       // Specify RAM data width
         .RAM_DEPTH(4096),                     // Specify RAM depth (number of entries)
         .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-        .INIT_FILE("/Users/cynthiazhang/Documents/Classes/FA23/205/6.111-Music-Transcription-Project/data/new_coeff.mem")          // Specify name/location of RAM initialization file if using one (leave blank if not)
+        .INIT_FILE("/home/zcynthia/final_folder/6.111-Music-Transcription-Project/data/coefficients.mem")          // Specify name/location of RAM initialization file if using one (leave blank if not)
     ) image_BROM (
         .addra(coeff_addr),     // Address bus, width determined from RAM_DEPTH
         .dina(0),       // RAM input data, width determined from RAM_WIDTH
