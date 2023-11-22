b5650b567865fee030dc414518a6a4f3967b8fb5
Cynthia Zhang <zchcynthia@gmail.com>
2023-11-21 20:13:33 -0500

    hanning better stop buggin

--- hdl/hanning_window.sv
+++ hdl/hanning_window.sv
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 `default_nettype none // prevents system from inferring an undeclared logic (good practice)
 
-//`ifdef SYNTHESIS
-//`define FPATH(X) `"X`"
-//`else /* ! SYNTHESIS */
-//`define FPATH(X) `"./data/X`"
-//`endif  /* ! SYNTHESIS */
+// `ifdef SYNTHESIS
+// `define FPATH(X) `"X`"
+// `else /* ! SYNTHESIS */
+// `define FPATH(X) `"./data/X`"
+// `endif  /* ! SYNTHESIS */
 
 module hanning_window #(
     parameter DATA_WIDTH = 8,
@@ -23,7 +23,7 @@ module hanning_window #(
 
     logic signed [7:0] in_sample_pipe;
 
-    logic signed [23:0] stored_coeff;
+    logic signed [24:0] stored_coeff;
 
     always_ff @(posedge clk_in) begin
         if (rst_in) begin
@@ -36,8 +36,8 @@ module hanning_window #(
             if (audio_sample_valid) begin
                 coeff_addr <= coeff_addr+1;
                 in_sample_pipe <= in_sample;
-                post_mult_shift <= (stored_coeff * in_sample_pipe) >>> 24;
-                out_sample <= post_mult_shift;
+                post_mult_shift <= (stored_coeff * in_sample_pipe) ;
+                out_sample <= post_mult_shift >>> 24;
                 hanning_sample_valid <= 1;
             end else begin
                 hanning_sample_valid <= 0;
@@ -50,7 +50,7 @@ module hanning_window #(
         .RAM_WIDTH(24),                       // Specify RAM data width
         .RAM_DEPTH(4096),                     // Specify RAM depth (number of entries)
         .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-        .INIT_FILE("/home/zcynthia/6.111-Music-Transcription-Project/data/coefficients.mem")          // Specify name/location of RAM initialization file if using one (leave blank if not)
+        .INIT_FILE("/Users/cynthiazhang/Documents/Classes/FA23/205/6.111-Music-Transcription-Project/data/coefficients.mem")          // Specify name/location of RAM initialization file if using one (leave blank if not)
     ) image_BROM (
         .addra(coeff_addr),     // Address bus, width determined from RAM_DEPTH
         .dina(0),       // RAM input data, width determined from RAM_WIDTH
