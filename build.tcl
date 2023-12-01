91be8c0b4ea770a6e9caf5485f47e157e28c7167
mochan <mochan@mit.edu>
2023-12-01 01:31:04 -0500

    pushing changes

--- build.tcl
+++ build.tcl
@@ -29,17 +29,16 @@ set_part $partNum
 
 # Read in all IP
 #read_ip ./ip/clk_wiz_7425/clk_wiz_7425.xci
-read_ip ./ip/clk_wiz_69632/clk_wiz_69632.xci
-#read_ip ./ip/clk_wiz_139264/clk_wiz_139264.xci
+#read_ip ./ip/clk_wiz_69632/clk_wiz_69632.xci
+read_ip ./ip/clk_wiz_139264/clk_wiz_139264.xci
 #read_ip ./ip/fir_compiler_10taps_69632clk/fir_compiler_10taps_69632clk.xci
 #read_ip ./ip/fir_compiler_10taps_139264clk/fir_compiler_10taps_139264clk.xci
-read_ip ./ip/fir_compiler_30taps_69632clk/fir_compiler_30taps_69632clk.xci
+#read_ip ./ip/fir_compiler_30taps_69632clk/fir_compiler_30taps_69632clk.xci
 #read_ip ./ip/fir_compiler_30taps_139264clk/fir_compiler_30taps_139264clk.xci
 #read_ip ./ip/fir_compiler_60taps_69632clk/fir_compiler_10taps_69632clk.xci
 #read_ip ./ip/fir_compiler_60taps_139264clk/fir_compiler_60taps_139264clk.xci
-read_ip ./ip/xfft_140/xfft_140.xci
-
-
+#read_ip ./ip/fir_compiler_0/fir_compiler_0.xci
+#read_ip ./ip/fir_many_139264/fir_many_139264.xci
 generate_target all [get_ips]
 synth_ip [get_ips]
 
