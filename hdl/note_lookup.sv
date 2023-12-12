d8916e8d876cb56e33fc94c1629427b84c09a349
mochan <mochan@mit.edu>
2023-12-11 22:24:42 -0500

    note lookup edits

--- hdl/note_lookup.sv
+++ hdl/note_lookup.sv
@@ -38,6 +38,8 @@ module note_lookup(
     end
 
     logic [4:0] counter;
+    logic [4:0] counter_minus;
+    assign counter_minus = counter -1;
     logic finding;
     always_ff @(posedge clk_in) begin
         if(rst_in)begin
@@ -50,18 +52,18 @@ module note_lookup(
                 note_index <= 0;
             end else begin
                 if(ready_in && ~finding)begin
-                    counter <= 0;
+                    counter <= 1;
                     finding <= 1'b1;
                 end
                 else if(finding)begin
                     if (counter > 21) begin
                         finding <= 0;
                         note_index <= 0;
-                        counter <= 0;
+                        counter <= 1;
                     end else begin
-                        if (bin_floor[counter+1] >= bin_index && bin_index >= bin_floor[counter]) begin
-                        note_index <= {1'b1,counter};
-                        finding <= 0;
+                        if (bin_index <= bin_floor[counter]) begin
+                            note_index <= {1'b1,counter_minus};
+                            finding <= 0;
                         end
                         counter <= counter + 1;
                     end
